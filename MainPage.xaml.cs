f38720d4e483253c3ee9113d6cab4adb49c8a5f5
rkamath1 <33260647+rkamath1@users.noreply.github.com>
2019-08-13 13:37:24 -0700

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -17,85 +17,57 @@ using Windows.System;
 using Windows.UI.Xaml.Shapes;
 using System.Diagnostics;
 using Windows.Storage.Pickers;
-using Xamarin.Forms;
-using System.Text;
-using Windows.UI.Xaml.Media.Imaging;
-using Windows.Storage.Streams;
-using ImageSource = Xamarin.Forms.ImageSource;
+using Windows.Storage.Provider;
 using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Photo_Library_App
+namespace PhotoLibraryApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Windows.UI.Xaml.Controls.Page
+    public sealed partial class MainPage : Page
     {
-
+        private ObservableCollection<string> current;
         public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            
         }
 
-    
-       // ObservableCollection<BitmapImage> pictures = new ObservableCollection<BitmapImage>();
-
-
-        
-
-        //public ObservableCollection<BitmapImage> CollectionsPageImage { get => collectionsPageImage; set => collectionsPageImage = value; }
-
-        public async void Import_Photos_Button_ClickAsync(object sender, RoutedEventArgs e)
+        private async void Import_Photos_Button_ClickAsync(object sender, RoutedEventArgs e)
         {
-            
             {
-                var picker = new Windows.Storage.Pickers.FileOpenPicker();
-                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-                picker.FileTypeFilter.Add(".jpg");
-                picker.FileTypeFilter.Add(".jpeg");
-                picker.FileTypeFilter.Add(".png");
-                picker.FileTypeFilter.Add(".bmp");
+                FileOpenPicker openPicker = new FileOpenPicker();
+                openPicker.ViewMode = PickerViewMode.Thumbnail;
+                openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                openPicker.FileTypeFilter.Add(".jpg");
+                openPicker.FileTypeFilter.Add(".jpeg");
+                openPicker.FileTypeFilter.Add(".png");
 
-                //Xamarin.Forms.StreamImageSource
+                StorageFile file = await openPicker.PickSingleFileAsync();
 
 
-                var files = await picker.PickMultipleFilesAsync();
-              
-
-                if (files.Count > 0)
+                if (file != null)
                 {
+                    // Application now has read/write access to the picked file
+                    this.textBlock.Text = "Picked photo: " + file.Name;
 
-                    // Application now has read/write access to the picked file(s)
-                    foreach (Windows.Storage.StorageFile file in files)
-                    {
-                        using(Windows.Storage.Streams.IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
-                        {
-                            Windows.UI.Xaml.Media.Imaging.BitmapImage bitmapImage = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
-                            bitmapImage.SetSource(fileStream);
-                            image1.Source = bitmapImage;
-                            
-
-                        }
-
-                       
-                    }
-                    
                 }
                 else
                 {
-                    //
+                    this.textBlock.Text = "Operation cancelled.";
                 }
 
             }
 
-           
+
         }
 
+        private void Album_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AlbumPage));
+        }
     }
-
- 
 }
-        
