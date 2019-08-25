1292eef9ddc2ad2218f1ce4503eddc920f619eb5
Satya P Javvadi <saphin.rao@gmail.com>
2019-08-25 12:30:29 -0700

    Show clecked image in photoview

--- PhotoViewPage.xaml.cs
+++ PhotoViewPage.xaml.cs
@@ -4,14 +4,18 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,21 +26,24 @@ namespace PhotoLibraryApp
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class PhotoViewPage : Page
-    {
-        public string TextContent { get; set; }
-
-        // Items for the flip view
-        public Collection<Picture> Items { get; set; } // = Picture.Collection;
-
-        public PhotoViewPage()
-        {
-            this.InitializeComponent();
-            this.Items = Picture.Collection;
-        }
+    {                
+        public ObservableCollection<Picture> Items { get; set; }
 
         private void Back_Button_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage));
         }
-    }   
+
+        public PhotoViewPage()
+        {
+            this.InitializeComponent();            
+        }        
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {           
+            this.Items = Picture.Collection;
+            flipView.SelectedIndex = Convert.ToInt32(e.Parameter);
+        }
+    }  
+         
 }
